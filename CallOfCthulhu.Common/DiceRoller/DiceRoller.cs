43777401799328024287b500102bb630d35d3cb0
esoemad5 <esoemad5@gmail.com>
2020-09-20 17:24:13 -0500

    Added a dice roller.

---
+++ CallOfCthulhu.Common/DiceRoller/DiceRoller.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallOfCthulhu.Common.DiceRoller
+{
+    public class DiceRoller
+    {
+        public static int RollDice(IEnumerable<Die> dice, int modifier = 0)
+        {
+            var output = 0;
+            foreach (var die in dice)
+                output += die.Roll();
+            output += modifier;
+            return output;
+        }
+
+        public static int RollDice(string input)
+        {
+            var splitInput = input.Split(new[] { '+', '-' });
+            if (splitInput.Length > 2)
+                throw new ArgumentException($"Invalid input in DiceRoller.RollDice({input}).");
+
+            try
+            {
+                var output = splitInput.Length == 2 ? int.Parse(splitInput[1]) : 0;
+
+                var splitDiceString = splitInput[0].ToLower().Split(new[] { 'd' });
+                if (splitDiceString.Length != 2)
+                    throw new ArgumentException();
+
+                var die = new Die(int.Parse(splitDiceString[1]));
+                var numberOfDice = int.Parse(splitDiceString[0]);
+                for (var i = 0; i < numberOfDice; i++)
+                    output += die.Roll();
+
+                return output;
+            }
+            catch(Exception)
+            {
+                throw new ArgumentException($"Invalid input in DiceRoller.RollDice({input}).");
+            }
+        }
+    }
+}
---
+++ CallOfCthulhu.Common/DiceRoller/Die.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallOfCthulhu.Common.DiceRoller
+{
+    public class Die
+    {
+        public readonly int NumberOfSides;
+
+        private readonly Random _random;
+
+        public Die(int numberOfSides)
+        {
+            NumberOfSides = numberOfSides;
+            _random = new Random();
+        }
+
+        public int Roll()
+        {
+            return _random.Next(1, NumberOfSides + 1);
+        }
+    }
+}
