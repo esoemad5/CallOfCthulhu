f1bb2da19e525d102ce3eb04e2dbaa37d322933f
esoemad5 <esoemad5@gmail.com>
2020-09-21 21:16:48 -0500

    Added a console app to run solo adventures.

---
+++ CallOfCthulhu.SoloAdventureConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using CallOfCthulhu.SoloAdventures.Interfaces;
+using CallOfCthulhu.SoloAdventures.Adventures;
+using CallOfCthulhu.SoloAdventures;
+
+namespace CallOfCthulhu.SoloAdventureConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var adventureRunner = new AdventureRunner(new InputGetter(), new OutputDisplayer());
+            adventureRunner.RunAdventure(new AloneAgainstTheFlames());
+        }
+    }
+
+    class InputGetter : IGetStringInput
+    {
+        public string GetInput()
+        {
+            return Console.ReadLine();
+        }
+    }
+
+    class OutputDisplayer : IDisplayStringOutput
+    {
+        public void DisplayOutput(string output)
+        {
+            Console.WriteLine(output);
+        }
+    }
+}
